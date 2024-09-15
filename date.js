36ce3865de2b991c6839dd53751df8eb0e573a11
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-09-16 04:09:02 +0500

    Add files via upload

---
+++ date.js
@@ -0,0 +1,21 @@
+//dates
+
+/*let date = new Date ()
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toString());*/
+
+function mydate(){
+const createdate= new Date(2001,4,20);
+console.log(createdate.toLocaleDateString())
+
+}
+mydate();
+
+const createdate = new Date("2020-02-23");
+console.log(createdate.toLocaleDateString());
+
