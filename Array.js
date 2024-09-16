83a3ed4469e5ef6671d0bf82e5de3fed72fd2719
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-09-16 18:21:01 +0500

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,20 @@
+const football =["ronado","messi","ronaldino"];
+
+const cricket = ["babar","imran","nazir"];
+
+const hostel = ["awais","munawar","khalid"]
+
+//football.push(cricket);
+//console.log(football[3]);
+
+//const fo_cri =football.concat(cricket);
+//console.log(fo_cri);
+
+const sprit =[...cricket, ...football, ...hostel];
+
+console.log(sprit[7]);
+
+
+
+
+
