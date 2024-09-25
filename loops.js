fd1a15958d7654efe56b20234f7615abde861af0
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-09-25 17:25:06 +0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,43 @@
+
+
+
+
+// for (let i = 1; i >= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// for(let i = 0; i < !0; i++){
+
+//     const element = i;
+    
+//     console.log(element);
+    
+// }
+ 
+// for(let clas = 0; clas < 10; clas++){
+
+//     const footbaler = clas;
+     
+//     console.log(footbaler);
+    
+// }
+
+// for (let index = 0; index <= 20; index++) {
+//     const element = index;
+//     if (index == 7) {
+//         console.log('detected 7')
+//         break;
+//     }
+//     console.log(element);
+    
+// }
+for (let index = 0; index < 15; index++) {
+    const element = index;
+    if(index == 9){
+        console.log('number 9 is selected');
+        continue;
+    }
+    console.log(element);
+    
+}
