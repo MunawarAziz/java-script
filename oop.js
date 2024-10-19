ec6e2dad837077d7cf2fbcd23c94801e11733320
MunawarAziz <149875349+MunawarAziz@users.noreply.github.com>
2024-10-19 14:46:13 +0500

    Add files via upload

---
+++ oop.js
@@ -0,0 +1,13 @@
+let user ={
+    userName : "Munawar",
+    logIn : 2,
+    pasword: 4567,
+
+    getuserdetail: function(userName, logIn, pasword){
+         // console.log(this.userName)
+    }
+
+  
+}
+  console.log(this)
+//console.log(user.userName)
